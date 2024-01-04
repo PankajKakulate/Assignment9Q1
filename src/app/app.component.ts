6a914eb997675572737a0d2af160b5841b63fdfe
pankaj <pankaj7187815@gmail.com>
2024-01-04 20:03:16 +0530

    i c 04-01-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MyRevPipe } from './my-rev.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MyRevPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Assignment9Q1';
+  public Name = "Marvellous";
 }
---
+++ src/app/my-rev.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'myRev',
+  standalone: true
+})
+export class MyRevPipe implements PipeTransform {
+
+  transform(value: string): string {
+
+    let temp : string="";
+      for(var i :number=value.length;i>=0;i--){
+        temp=temp+value.charAt(i);
+      }
+    return temp;
+  }
+
+}
